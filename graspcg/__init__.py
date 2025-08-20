47832bc4122e89f44836f79ca1834ed5e72de5ec
timodelgado <41348202+timodelgado@users.noreply.github.com>
2025-08-20 09:49:16 -0700

    Delete graspcg directory

--- graspcg/__init__.py
+++
@@ -1,8 +0,0 @@
-# graspcg/__init__.py
-"""
-GRASP-CG reconstruction package.
-"""
-__version__ = "0.1.0"
-
-# Optional: expose common submodules at top level
-from . import workspace, ops, solvers, utils, nufft, numerics
--- graspcg/utils/debug.py
+++
@@ -1,8 +0,0 @@
-import torch
-
-def check_memory(step):
-    print(f"{step} - Torch Memory allocated: {torch.cuda.memory_allocated() / (1024 * 1024):.2f} MiB")
-    print(f"{step} - Torch Memory reserved: {torch.cuda.memory_reserved() / (1024 * 1024):.2f} MiB")
-    #print(f"{step} - Cupy Memory used (allocated): {cp.get_default_memory_pool().used_bytes() / (1024 * 1024):.2f} MiB")
-    #print(f"{step} - Cupy Memory reserved: {cp.get_default_memory_pool().total_bytes() / (1024 * 1024):.2f} MiB")
-    print(f"{step} - Free Memory: {torch.cuda.mem_get_info()[0] / (1024 * 1024):.2f} MiB")
